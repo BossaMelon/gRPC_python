92e9e39cd8450154a0123182f7647665e8f619e8
BossaMelon <wyh453255505@gmail.com>
2019-11-26 22:40:10 +0100

    first

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import time
 
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('192.168.2.118:50051')
     # 设定发送ip和port
 
     stub = first_pb2_grpc.FirstGreeterStub(channel)
@@ -16,10 +16,10 @@ def run():
         response = stub.SayHello(first_pb2.HelloRequest(name=' you'))
         # 对象.service对应功能(message文件.对应message(字段赋值))
         # 类似调用service的函数
-        response2 = stub.SayNum(first_pb2.NumRequest())
+        # response2 = stub.SayNum(first_pb2.NumRequest())
 
         print("Greeter client received: " + response.message)
-        print("Length= : " + str(response2.num))
+        # print("Length= : " + str(response2.num))
 
         time.sleep(0.5)
 
